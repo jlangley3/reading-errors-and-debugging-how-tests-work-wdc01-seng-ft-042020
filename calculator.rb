28f54e82f7c15558a6a3c4d15200b95c73c48454
Jesse Langley <jlangleyiii@aol.com>
2020-04-08 22:58:42 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
  first_number = 1 # Add your variables here
 second_number = 2 
 sum = first_number + second_number
-difference = second_number - first_number
+difference = first_number - second_number
